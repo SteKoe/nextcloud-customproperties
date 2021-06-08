82c0d3e21338247799f1d154c76d44d3788b4db0
Stephan Köninger <github@stekoe.de>
2021-06-09 00:05:53 +0200

    Feat/workflow (#24)

--- build.sh
+++ build.sh
@@ -4,71 +4,66 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-NEXTCLOUD_VERSION=21
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+VERSION=$(xmllint --xpath 'string(/info/version)' appinfo/info.xml)
+CONTAINER_ID=
 
 main() {
-  local version
-  version=$1
-
-  downloadAndExtract $version
   buildJavaScript
   signSource
   buildArchive
   createSignature
-
-  rm v$version.tar.gz
-  rm -rf customproperties
-}
-
-downloadAndExtract() {
-  local version
-  version=$1
-
-  wget https://github.com/SteKoe/nextcloud-customproperties/archive/v$version.tar.gz -O v$version.tar.gz
-  tar -zxf v$version.tar.gz
-
-  mv nextcloud-customproperties-$version customproperties
+  tidyUp
 }
 
 buildJavaScript() {
   echo "Build JavaScript..."
-  (cd customproperties/src && npm ci && npm run build)
+  npm i && npm run build
 }
 
 signSource() {
-  local cid
+  CONTAINER_ID=$(docker run -d --name customproperties-build -v ~/.nextcloud/certificates/:/nextcloud/certificates/ nextcloud)
 
-  rm -rf customproperties/.git customproperties/src
+  echo "Copy app to container '$CONTAINER_ID'..."
+  docker cp $(pwd) $CONTAINER_ID:/customproperties
+  docker exec -i $CONTAINER_ID /bin/bash -c "cd /customproperties && rm -rf .cache .git .idea node_modules"
 
-  cid=$(docker run -d -v $(pwd)/customproperties:/app -v ~/.nextcloud/certificates/:/nextcloud/certificates/ nextcloud:$NEXTCLOUD_VERSION)
-  echo "Sign sources using container '$cid'..."
+  echo "Sign sources using container '$CONTAINER_ID'..."
 
   echo "Waiting for NextCloud to be initialized..."
-  until docker logs $cid 2>&1 | grep "Initializing finished" >/dev/null; do
-    sleep 1
+  until docker logs $CONTAINER_ID 2>&1 | grep "Initializing finished" >/dev/null; do
+    sleep 5
     echo "Waiting..."
   done
   echo "NextCloud initialized!"
 
-  echo "Signing app..."
-  docker exec -i $cid /bin/bash -c "chmod -R +w /app" || true
-  docker exec -i $cid php occ integrity:sign-app --privateKey=/nextcloud/certificates/customproperties.key --certificate=/nextcloud/certificates/customproperties.crt --path=/app || true
-  echo "Tidy up..."
-  docker rm -f $cid >/dev/null
+  echo "Sign app app..."
+  docker exec -i $CONTAINER_ID php occ integrity:sign-app --privateKey=/nextcloud/certificates/customproperties.key --certificate=/nextcloud/certificates/customproperties.crt --path=/customproperties || true
 }
 
 buildArchive() {
   echo "Build archive..."
-  (tar -czf customproperties.tar.gz \
+  docker exec -i $CONTAINER_ID tar -czf customproperties_$VERSION.tar.gz \
     --exclude=./src/node_modules/ \
     --exclude=./.git/ \
+    --exclude=./.build/ \
+    --exclude=./.cache/ \
+    --exclude=./.idea/ \
     --exclude=./src/ \
-    customproperties/)
+    /customproperties
+
+  echo "Copy archive..."
+  docker cp $CONTAINER_ID:/var/www/html/customproperties_$VERSION.tar.gz $(pwd)
 }
 
 createSignature() {
   echo "Create signature..."
-  openssl dgst -sha512 -sign ~/.nextcloud/certificates/customproperties.key customproperties.tar.gz | openssl base64
+  openssl dgst -sha512 -sign ~/.nextcloud/certificates/customproperties.key customproperties_$VERSION.tar.gz | openssl base64
+}
+
+tidyUp() {
+  echo "Tidy up..."
+  docker rm -f -v $CONTAINER_ID >/dev/null
 }
 
 main $@
