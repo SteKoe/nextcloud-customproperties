66d5d048971709d2b2817497a218aafde25219fe
p-bo <pavel.borecki@gmail.com>
2021-07-24 15:10:18 +0200

    Add Czech localization - .js variant (#30)

---
+++ l10n/cs.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+    "customproperties",
+    {
+    "Custom Properties" : "Uživatelsky určená metadata",
+    "Files app plugin to add custom properties to files and folders" : "Zásuvný modul pro Soubory, který umožňuje přidávat k souborům a složkám uživatelsky určená metadata.",
+    "Custom Properties defined here are available to all users. They are shown in \"Properties\" tab in sidebar view. They can be accessed via WebDAV. Deleting properties will not wipe property values." : "Uživatelsky určená metadata zde jsou přístupná všem uživatelům. Jsou zobrazena na záložce „Vlastnosti“ v postranním panelu. Je možné k nim přistupovat i přes WebDAV. Smazání vlastností nevymaže jejich hodnoty.",
+    "Properties" : "Metadata",
+    "Custom Property has been deleted!" : "Uživatelsky určená vlastnost byla smazána!",
+    "Custom Property has been changed!" : "Uživatelsky určená vlastnost byla změněna!",
+    "New Custom Property has been added!" : "Nová uživatelsky určená vlastnost byla přidána!",
+    "Error saving property, please check constraints." : "Chyba při ukládání vlastnosti, zkontrolujte omezení!",
+    "Add" : "Přidat",
+    "Cannot create Custom Property. The given input is invalid." : "Uživatelsky určená metadata se nedaří vytvořit. Zadaný vstup není platný.",
+    "Name..." : "Název…",
+    "Label..." : "Štítek…",
+    "Update" : "Uložit",
+    "Delete" : "Smazat",
+    "No Custom Properties defined, yet." : "Zatím nezadána žádná uživatelsky určená metadata.",
+    "WebDAV properties" : "WebDAV-metadata"
+},
+"");
